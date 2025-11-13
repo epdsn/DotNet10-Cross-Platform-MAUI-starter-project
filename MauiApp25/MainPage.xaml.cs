ac816ece346dd8a4559b6e27a23226de10e15df8
epdsn <ibestudios@gmail.com>
2025-11-13 14:49:29 -0700

    Updated some styles

--- MauiApp25/MainPage.xaml.cs
+++ MauiApp25/MainPage.xaml.cs
@@ -4,13 +4,18 @@ namespace MauiApp25
 {
     public partial class MainPage : ContentPage
     {
+        private readonly Services.IAuthService _authService;
+
         public MainPage()
         {
             InitializeComponent();
 
+            _authService = App.Services.GetService<Services.IAuthService>()!;
+
             // Wire up navigation buttons
             var loginButton = this.FindByName<Button>("LoginButton");
             var signupButton = this.FindByName<Button>("SignupButton");
+            var logoutButton = this.FindByName<Button>("LogoutButton");
 
             if (loginButton != null)
                 loginButton.Clicked += async (s, e) => await Navigation.PushAsync(App.Services.GetService<Views.LoginPage>()!);
@@ -20,6 +25,35 @@ namespace MauiApp25
                     var page = App.Services.GetService<Views.LoginPage>()!;
                     await Navigation.PushAsync(page);
                 };
+
+            if (logoutButton != null)
+            {
+                logoutButton.Clicked += async (s, e) =>
+                {
+                    await _authService.SignOutAsync();
+                };
+            }
+
+            // Subscribe to auth state changes to update button visibility
+            _authService.AuthStateChanged += OnAuthStateChanged;
+            UpdateButtons();
+        }
+
+        private void OnAuthStateChanged()
+        {
+            MainThread.BeginInvokeOnMainThread(UpdateButtons);
+        }
+
+        private void UpdateButtons()
+        {
+            var loginButton = this.FindByName<Button>("LoginButton");
+            var signupButton = this.FindByName<Button>("SignupButton");
+            var logoutButton = this.FindByName<Button>("LogoutButton");
+
+            var isSignedIn = _authService.IsSignedIn;
+            if (loginButton != null) loginButton.IsVisible = !isSignedIn;
+            if (signupButton != null) signupButton.IsVisible = !isSignedIn;
+            if (logoutButton != null) logoutButton.IsVisible = isSignedIn;
         }
     }
 }
